4f310897e5e6cd226a502498b1187e36105adaec
fll <fllning@163.com>
2023-03-12 01:33:46 +0800

    feat：脚手架安装，项目搭建

---
+++ sign-online-management/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    hello world
+    <el-button>我是按钮</el-button>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/reset.scss';
+@import '@/assets/styles/iconfont.scss';
+@import '@/assets/styles/common.scss';
+</style>
---
+++ sign-online-management/src/main.ts
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+// 引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 中文
+// import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+// 1.引入ICON图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+// 2.icon配置
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.use(ElementPlus)
+app.use(store)
+app.use(router).mount('#app')
