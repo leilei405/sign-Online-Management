875d53cea7e143f5725053852d70e898a2b19987
fll <fllning@163.com>
2023-03-13 21:52:43 +0800

    feat：收集登录数据及表单校验规则

--- sign-online-management/src/views/Login/Login.vue
+++ sign-online-management/src/views/Login/Login.vue
@@ -20,9 +20,21 @@
                 <el-input v-model="ruleForm.pass" type="password"  placeholder="请输入密码" />
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="submitForm(ruleFormRef)" auto-insert-space>登录</el-button>
+                <el-button class="btnLogin" type="primary" @click="submitForm(ruleFormRef)" auto-insert-space>登录</el-button>
             </el-form-item>
         </el-form>
+        <!-- 测试账号区域 -->
+        <div class="usersTest">
+            <el-row :gutter="20">
+                <el-col :span="12" v-for="item in testUser" :key="item.email">
+                    <h3>
+                        <div>测试账号{{ i++ }}</div>
+                        <el-button type="primary" @click="testLogin({email: item.email, pass: item.pass})">一键登录</el-button></h3>
+                    <p>邮箱：{{ item.email }}</p>
+                    <p>密码：{{ item.pass }}</p>
+                </el-col>
+            </el-row>
+        </div>
     </div>
 </template>
 
@@ -33,42 +45,68 @@ import type { FormInstance, FormRules } from 'element-plus'
 defineComponent({
     name: "LoginView",
 })
-// 定义类型
+// 测试账号序列
+let i = 1;
+
+// 定义类型 email pass
 interface User {
     email: string;
     pass: string;
 }
+
 // 实例
 const ruleFormRef = ref<FormInstance>()
+
 // 所需字段
 const ruleForm = reactive<User>({
   pass: '',
   email: ''
 })
+
 // 表单校验规则
 const rules = reactive<FormRules>({
-  
+    email: [
+        { required: true, message: '请输入邮箱', trigger: 'blur' },
+        { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur' }
+    ],
+    pass: [
+        { required: true, message: '请输入密码', trigger: 'blur' }
+    ]
 })
+
 // 点击登录
 const submitForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return
   formEl.validate((valid) => {
     if (valid) {
-      console.log('submit!', valid)
+      console.log('submit!', ruleForm)
     } else {
       console.log('error submit!')
       return false
     }
   })
 }
-// const store = useStore();
 
-// store.dispatch('users/login').then((res) => {
-//     console.log(res);
-// })
+// 测试登录账号信息
+const testUser: User[] = [
+    {
+        email: 'huangrong@imooc.com',
+        pass: 'huangrong'
+    },
+    {
+        email: 'hongqigong@imooc.com',
+        pass: 'hongqigong'
+    },
+]
+
+// 测试账号点击登录事件
+const testLogin = (user: User) => {
+    ruleForm.email = user.email;
+    ruleForm.pass = user.pass;
+    submitForm(ruleFormRef.value)
+}
 
 </script>
-
 <style scoped lang="scss">
 .login {
     width: 100vw;
@@ -100,8 +138,19 @@ const submitForm = (formEl: FormInstance | undefined) => {
          margin: 0 auto;
          margin-top: 20px;
     }
-    .el-button {
+    .btnLogin {
         width: 100%;
+    }
+    .usersTest { 
+        width: 500px;
+        margin: 60px auto;
+        color: rgba(0, 0, 0, 0.5)
+    }
+    h3 {
+        font-size: 16px;
+    }
+    p {
+        margin: 20px;
     }
 }
 </style>
