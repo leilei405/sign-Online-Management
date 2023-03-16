af7b39eac371523d4b18d5ac262510291886cba9
fll <fllning@163.com>
2023-03-16 23:42:45 +0800

    feat： 月份控件联动描述列表和日历

--- sign-online-management/src/views/Sign/Sign.vue
+++ sign-online-management/src/views/Sign/Sign.vue
@@ -2,17 +2,14 @@
     <div>
         <el-descriptions border direction="vertical" :column="9">
             <el-descriptions-item label="月份">{{ month }}月</el-descriptions-item>
-            <el-descriptions-item label="正常出勤">0</el-descriptions-item>
-            <el-descriptions-item label="正常出勤">0</el-descriptions-item>
-            <el-descriptions-item label="正常出勤">0</el-descriptions-item>
-            <el-descriptions-item label="正常出勤">0</el-descriptions-item>
-            <el-descriptions-item label="正常出勤">0</el-descriptions-item>
-            <el-descriptions-item label="正常出勤">0</el-descriptions-item>
+            <el-descriptions-item v-for="value, key in DetailKey" :key="key" :label="value">
+                {{ detailValue[key] }}
+            </el-descriptions-item>
             <el-descriptions-item label="操作">
-                <el-button type="primary" plain size="small">查看详情</el-button>
+                <el-button type="primary" plain size="small" @click="handleException">查看详情</el-button>
             </el-descriptions-item>
             <el-descriptions-item label="考勤状态">
-                <el-button type="danger" plain size="small">异常</el-button>
+                <el-button :type="detailState.type" plain size="small">{{ detailState.text }}</el-button>
             </el-descriptions-item>
         </el-descriptions>
         <el-calendar v-model="date">
@@ -20,7 +17,7 @@
                 <el-button type="primary" size="small">在线签到</el-button>
                 <el-space>
                     <el-button plain>{{ year }}年</el-button>
-                    <el-select v-model="month">
+                    <el-select v-model="month" @change="handleChange">
                         <el-option v-for="item in 12" :value="item" :key="item" :label="item + '月'" />
                     </el-select>
                 </el-space>
@@ -31,13 +28,51 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+const store = useRouter();
+
+const  handleException = () => {
+    store.push('/exception');
+}
+
 defineComponent({
     name: "SignView",
 })
+
 const date = ref(new Date());
 const year = date.value.getFullYear();
 const month = ref(date.value.getMonth() + 1)
+
+enum DetailKey {
+    normal = '正常出勤',
+    absent = '旷工',
+    miss = '漏打卡',
+    late = '迟到',
+    early = '早退',
+    lateAndEarly = '迟到并早退',
+}
+
+const detailValue = reactive({
+    normal: 0,
+    absent: 0,
+    miss: 0,
+    late: 0,
+    early: 0,
+    lateAndEarly: 0
+})
+
+const detailState = reactive({
+    type: 'success' as 'success' | 'danger',
+    text: '正常' as '正常' | '异常'
+})
+
+// 点击月份切换动态日历   没太懂 回头再看一下
+const handleChange = () => {
+    date.value = new Date(`${year}-${month.value}`)
+}
+
+
 </script>
 
 <style scoped lang="scss">
